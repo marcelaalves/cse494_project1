ef1d9f388fffa179c790668ccba44e32914b3840
rjtorr1 <rjtorr1@cpcom102macm26.care.asu.edu>
2014-10-28 19:31:23 -0700

    Initial Commit

---
+++ Project_1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Project_1
+//
+//  Created by rjtorr1 on 10/28/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Project_1/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Project_1
+//
+//  Created by rjtorr1 on 10/28/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
